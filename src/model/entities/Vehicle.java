07464d3680a38dee6b51372b6b186b85536c0ff8
Bruno Frazao <bruno.consultoriarj@gmail.com>
2024-09-29 18:18:43 -0300

    Solution without interface

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,24 @@
+package model.entities;
+
+public class Vehicle {
+	
+	private String Vehicle;
+
+	
+	public Vehicle() {
+	}
+
+	public Vehicle(String vehicle) {
+		Vehicle = vehicle;
+	}
+	
+
+	public String getVehicle() {
+		return Vehicle;
+	}
+
+	public void setVehicle(String vehicle) {
+		Vehicle = vehicle;
+	}
+	
+}
